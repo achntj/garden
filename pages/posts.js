92fb8c5e5a930cea04b145878dc458e4fcce68b5
achintyajha <achintyakjha@gmail.com>
2022-04-24 13:02:12 +0530

    svg arrows + index CTAs

--- pages/posts.js
+++ pages/posts.js
@@ -35,7 +35,23 @@ export default function Posts({ posts }) {
                       <b>{post.frontmatter.title}</b>
                     </span>
                     <span className="ml-2 transition-[margin] group-hover:ml-5">
-                      &rarr;
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="inline-block text-slate-600 dark:text-slate-400"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 16 26"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <line x1="5" y1="12" x2="19" y2="12" />
+                        <line x1="13" y1="18" x2="19" y2="12" />
+                        <line x1="13" y1="6" x2="19" y2="12" />
+                      </svg>
                     </span>
                     <br />
                     {post.frontmatter.description}
