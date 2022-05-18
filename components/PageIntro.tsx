49b6ac3b54f46552bb080ad579261f49c4eab623
achintyajha <achintyakjha@gmail.com>
2022-05-18 10:57:52 +0530

    reduce gradient banding

--- components/PageIntro.tsx
+++ components/PageIntro.tsx
@@ -1,7 +1,7 @@
 export default function PageIntro({ children }) {
   return (
     <div className="relative">
-      <div className="absolute -inset-px opacity-50 dark:bg-gradient-to-r dark:from-white dark:to-pink-600 bg-gradient-to-r from-[#dcd5fcec] via-[#c7fdf1d0] to-[#d3c8ffec] rounded-lg border-[1px] dark:blur transition duration-500"></div>
+      <div className="absolute -inset-px opacity-50 dark:bg-gradient-to-r dark:from-white dark:to-pink-600 bg-gradient-to-r from-[#dcd5fc] via-[#c7fdf1] to-[#d3c8ff] rounded-lg border-[1px] dark:blur transition duration-500"></div>
       <div className="relative dark:bg-[#050505] rounded-lg p-8 mb-10 sm:flex items-end justify-between">
         {children}
       </div>
