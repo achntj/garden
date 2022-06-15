a9bde148d249005e8c93cfca7c2dee46a1f09cf8
achintyajha <achintyakjha@gmail.com>
2022-06-15 12:44:35 +0530

    nav home svg + project stack arrays

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,40 +1,27 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
-import { useState } from "react";
 import ThemeSwitcher from "./ThemeSwitcher";
 
-function cn(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
 function Nav() {
-  const [isLoading, setLoading] = useState(true);
   return (
-    <div className="rounded-b drop-shadow-lg py-4">
+    <div className="rounded-b drop-shadow-lg py-4 mt-2">
       <div className="sm:flex justify-between items-center">
         <div className="">
-          <p className="">
-            <Link passHref href="/">
-              <a>
-                <Image
-                  alt="site avatar"
-                  src={`/images/avatar.png`}
-                  width={40}
-                  height={40}
-                  quality={100}
-                  priority
-                  className={cn(
-                    "duration-700 ease-in-out sm:mb-0 rounded select-none",
-                    isLoading
-                      ? "grayscale blur-2xl scale-110"
-                      : "grayscale-0 blur-0 scale-100"
-                  )}
-                  onLoadingComplete={() => setLoading(false)}
-                />
-              </a>
-            </Link>
-          </p>
+          <Link passHref href="/">
+            <svg
+              width="20"
+              height="20"
+              viewBox="10 10 44 44"
+              xmlns="http://www.w3.org/2000/svg"
+              className="cursor-pointer text-base"
+            >
+              <path
+                fillRule="evenodd"
+                fill="currentColor"
+                d="M48.01 10.02H48c1.1-.01 2 .89 2 1.99v8.52l0-.01c-.01.41-.34.74-.76.74 -.2-.01-.39-.08-.53-.22l-6.14-6.14 -.01-.01c-.38-.38-.59-.89-.59-1.42v-1.51l0 0c-.01-1.11.89-2.01 1.99-2.01Zm0 44h-9H39c-1.11-.01-2-.9-2-2.01v-12 0c0-1.11-.9-2-2-2h-6 -.01c-1.11 0-2 .89-2 2v12 0c0 1.1-.9 2-2 2h-9 -.01c-1.11-.01-2-.9-2-2.01V33.98H9.99l-.01-.001c-1.11-.01-2.01-.9-2-2.01 0-.54.21-1.04.58-1.42L30.55 8.53l-.01 0c.78-.79 2.04-.79 2.82-.01 0 0 0 0 0 0l21.994 22 0 0c.78.78.78 2.04-.01 2.82 -.38.37-.89.58-1.42.58h-4v18.01l0 0c0 1.1-.9 2-2 2 -.01 0-.01 0-.01-.001Z"
+              ></path>
+            </svg>
+          </Link>
         </div>
 
         <nav
