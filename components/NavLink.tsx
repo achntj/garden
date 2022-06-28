8560f8b80eae465b2b492801ed301f346e17c5d1
achintyajha <achintyakjha@gmail.com>
2022-06-28 10:31:51 +0530

    isActive to nav

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function NavLink({ children, href, name }) {
+  const router = useRouter();
+  const isActive = router.asPath === href;
   return (
     <Link passHref href={href}>
-      <div className="navLink flex items-center space-x-4 cursor-pointer hover:bg-white dark:hover:bg-zinc-700 rounded-lg transition p-2">
+      <div
+        className={`${
+          isActive ? "font-semibold dark:text-white dark:bg-neutral-600" : " "
+        } navLink flex items-center space-x-4 cursor-pointer hover:bg-white dark:hover:bg-neutral-600 rounded-lg transition p-2`}
+      >
         {children}
         <p className="hidden sm:block">{name}</p>
       </div>
