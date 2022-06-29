bc8a2890170de389d954bcd7129d6a64bab509a4
achintyajha <achintyakjha@gmail.com>
2022-06-29 19:20:29 +0530

    hid cross icon div on lg

--- components/Base.tsx
+++ components/Base.tsx
@@ -9,11 +9,8 @@ export default function Base({ children, ...pageProps }) {
   return (
     <>
       <div className="dark:bg-[#101010] dark:bg-none prose-a:text-cyan-600 prose-a:no-underline prose-a:transition dark:prose-a:text-zinc-300 hover:prose-a:text-cyan-800 dark:hover:prose-a:text-pink-600 dark:prose-headings:text-slate-200 dark:prose-strong:text-zinc-300 prose-blockquote:border-emerald-400 dark:prose-blockquote:text-zinc-300">
-        <div className="p-5 shadow-xl">
-          <MenuIcon
-            onClick={() => setOpen(true)}
-            className="h-6 w-6 sm:hidden"
-          />
+        <div className="p-5 sm:hidden">
+          <MenuIcon onClick={() => setOpen(true)} className="h-6 w-6" />
         </div>
         <div className="hidden sm:block">
           <Nav />
