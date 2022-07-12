0d9c96851158990784098e13b23219f286c5741c
Kapil Chaudhary <kapil.math.hons@gmail.com>
2022-07-13 01:14:34 +0530

    fixing-container

--- pages/guestbook.tsx
+++ pages/guestbook.tsx
@@ -6,8 +6,12 @@ export default function GuestbookPage({ fallbackData }) {
     <>
       <Container 
        title="Guestbook"
-       description=""
+       description="An artifact of the 90s webs. Feel free to write anything!"
        >
+       <div>
+        <h1 className="mb-0">Guestbook</h1>
+        <p>An artifact of the 90s webs. Feel free to write anything!</p>
+      </div> 
       <div className="mx-auto justify-center">
         {/* <h1 className="mb-4 text-3xl font-bold 
       tracking-tight text-black dark:text-white md:text-5xl">
@@ -17,14 +21,6 @@ export default function GuestbookPage({ fallbackData }) {
         Leave a comment below. It could be anything – appreciation, information, wisdom, or even
         humor. Surprise me!
       </p> */}
-        <div className="space-y-2 pt-6 pb-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-            Guestbook
-          </h1>
-          <p className="text-md leading-7 text-gray-500 dark:text-gray-400">
-            An artifact of the 90s webs. Feel free to write anything!
-          </p>
-        </div>
         <Guestbook fallbackData={fallbackData} />
       </div>
       </Container>
